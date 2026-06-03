a1d6f0f706428ff09505aae66a0a2f7c3b987241
BodhiSmith13 <cooperjamessmith10@gmail.com>
2026-06-03 14:48:01 -0400

    Added debug messages Added a password scoring system, which is used to end the program when a five out of five password is created. Scoring is based on character variety, length, and whether or not the password is a pass phrase. Cleaned up text

--- main.py
+++ main.py
@@ -1,67 +1,50 @@
 import enchant
 
+debug = False
 d = enchant.Dict("en_US")
 # Calculates how long it would take to crack password and uses appropriate units
 def crack_time (combos):
     print("The average computer can make 100 billion attempts a second at cracking your password.")
     time_to_guess = combos / 100000000000
-    if time_to_guess < 60:
-        print(f"It would take the average computer {time_to_guess:,.4f} seconds at most to guess your password.\n"
-              f"On average, it would take {time_to_guess / 2:,.4f} seconds.")
-    elif time_to_guess >= 3154000000:
+    if time_to_guess >= 3154000000:
         print(f"It would take the average computer {time_to_guess / 3154000000:,.4f} centuries at most to guess your "
-              f"password.\nOn average, it would take {time_to_guess / 3154000000 / 2:,.4f} centuries.")
+              f"password.\nOn average, it would take {time_to_guess / 3154000000 / 2:,.4f} centuries.\n")
     elif time_to_guess >= 315400000:
         print(f"It would take the average computer {time_to_guess / 315400000:,.4f} decades at most to guess your "
-              f"password.\nOn average, it would take {time_to_guess / 315400000 / 2:,.4f} decades.")
+              f"password.\nOn average, it would take {time_to_guess / 315400000 / 2:,.4f} decades.\n")
     elif time_to_guess >= 31540000:
         print(
             f"It would take the average computer {time_to_guess / 31540000:,.4f} years at most to guess your password."
-            f"\nOn average, it would take {time_to_guess / 31540000 / 2:,.4f} years.")
+            f"\nOn average, it would take {time_to_guess / 31540000 / 2:,.4f} years.\n")
     elif time_to_guess >= 2628000:
         print(
             f"It would take the average computer {time_to_guess / 2628000:,.4f} months at most to guess your password."
-            f"\nOn average, it would take {time_to_guess / 2628000 / 2:,.4f} months.")
+            f"\nOn average, it would take {time_to_guess / 2628000 / 2:,.4f} months.\n")
     elif time_to_guess >= 604800:
         print(
             f"It would take the average computer {time_to_guess / 604800:,.4f} weeks at most to guess your password.\n"
-            f"On average, it would take {time_to_guess / 604800 / 2:,.4f} weeks.")
+            f"On average, it would take {time_to_guess / 604800 / 2:,.4f} weeks.\n")
     elif time_to_guess >= 86400:
         print(f"It would take the average computer {time_to_guess / 86400:,.4f} days at most to guess your password\n"
-              f"On average, it would take {time_to_guess / 86400 / 2:,.4f} days.")
+              f"On average, it would take {time_to_guess / 86400 / 2:,.4f} days.\n")
     elif time_to_guess >= 3600:
         print(f"It would take the average computer {time_to_guess / 3600:,.4f} hours at most to guess your password\n"
-              f"On average, it would take {time_to_guess / 3600 / 2:,.4f} hours.")
+              f"On average, it would take {time_to_guess / 3600 / 2:,.4f} hours.\n")
     elif time_to_guess >= 60:
         print(f"It would take the average computer {time_to_guess / 60:,.4f} minutes at most to guess your password\n"
-              f"On average, it would take {time_to_guess / 60 / 2:,.4f} minutes.")
+              f"On average, it would take {time_to_guess / 60 / 2:,.4f} minutes.\n")
+    else:
+        print(f"It would take the average computer {time_to_guess:,.4f} seconds at most to guess your password.\n"
+              f"On average, it would take {time_to_guess / 2:,.4f} seconds.\n")
 
 # For every character in the string, start there. From there, iterate up the string, building a larger substring
 # every time. If the substring is a word, save it. Once every character has been through this process, rearrange all words
 # until a sentence is formed. Then, remove all spaces from that sentence, and compare it to the input. If they are the same
 #, an appropriate pass phrase has been inputted
 
-def display_list (array):
-    for item in array:
-        print(item, end=", ")
-    print()
-
-def mash (array):
-    string = ""
-    for item in array:
-        string += item
-    return string
-
-def remove_duplicants (array):
-    i = 0
-    output = []
-    while i < len(array):
-        if array[i] not in output:
-            output.append(array[i])
-        i += 1
-    return output
-
 def is_phrase (string):
+    if not string.isalpha():
+        return False
     words = []
     i = 0
     while i < len(string) + 1:
@@ -74,68 +57,71 @@ def is_phrase (string):
             j += 1
         i += 1
 
-    print("Words:")
-    display_list(words)
+    if debug:
+        print(f"Words:")
+        print(", ".join(words))
 
     starter_words = []
     for word in words:
         if word[0] == string[0]:
             starter_words.append(word)
-    starter_words = remove_duplicants(starter_words)
+    starter_words = list(dict.fromkeys(starter_words))
 
-    print("Starter Words:")
-    display_list(starter_words)
+    if debug:
+        print("Starter Words:")
+        print(", ".join(starter_words))
     if len(starter_words) > 0:
         for starter_word in starter_words:
             result = build_phrase(words, starter_word, string)
             if result:
-                print(f"Result: {result}")
-                break
+                if debug:
+                    print(f"Result: {result}")
+                return True
+        return False
+    else:
+        return False
 
 
 def build_phrase (words, starter_word, string):
     new_words = []
-    print(f"Starting with {mash(starter_word)}")
+    if debug:
+        print(f"Starting with {"".join(starter_word)}")
     i = 0
     for word in words:
         i += 1
-        print(f"Looking at {word}, word {i} in the word list")
+        if debug:
+            print(f"Looking at {word}, word {i} in the word list")
         if string[0:len(starter_word + word)] == starter_word + word:
-            print(f"Appending {word}")
+            if debug:
+                print(f"Appending {word}")
             new_words.append(starter_word + word)
-            print(f"{string[0:len(new_words[-1])]}:{new_words[-1]}")
-    display_list(new_words)
+            if debug:
+                print(f"{string[0:len(new_words[-1])]}:{new_words[-1]}")
+    if debug:
+        print(", ".join(new_words))
     if string in new_words:
-        return "Phrase found"
+        return True
     for new_word in new_words:
         result = build_phrase(words, new_word, string)
         if result:
             return result
-    return ""
+    return False
 
-
-
-test = "isitin"
-is_phrase(test)
-
+if not debug:
+    debug = input("Do you want debug enabled? Input anything for yes, or press enter to continue.")
 
 # Introductory message
+print("Welcome to Password Strength Checker! Your goal is to create a five star password.")
 print("Password guidelines:\n"
       "1. Make your password at least 15 characters long.\n"
       "2. Include a variety of character types.\n"
-      "3. Avoid passwords that are entirely numbers.\n")
+      "3. Make your password a passphrase, such as \"Sharksseekblood.\"\n")
 
 # Loops until the user decides to exit
 while True:
 
     # Asks user for password
     password = input("Enter your password, or enter exit to cancel: ")
-
-    # Initializes score for password
-    totalCombos = 0
-    letterCombos = 0
-    alphanumericCombos = 0
-    symbolCombos = 0
 
     # Initializes lists of punctuation and symbol characters
     punctuationList = ['.', '!', '?', ',', ';', ':', "\"", "\'"]
@@ -148,12 +134,9 @@ while True:
     punctuation = 0
     symbols = 0
     unidentified = 0
-    lowercaseCombo = 0
-    uppercaseCombo = 0
-    digitsCombo = 0
-    punctuationCombo = 0
-    symbolsCombo = 0
     unidentifiedCombo = 0
+    totalCombos = 0
+    score = 0
 
     # Tallies the number of characters present of each type
     for char in password:
@@ -169,6 +152,8 @@ while True:
             symbols += 1
         else:
             unidentified += 1
+    if unidentified != 0:
+        unidentifiedCombo = (95 - 52 - 10 - len(punctuationList) - len(symbolList)) ** unidentified
 
     # Tells the user how much of each character type their password has
     print("\nYour password has:\n"
@@ -181,47 +166,43 @@ while True:
 
     # For each character type, checks if that character type is present. If so, then calculates the total number of
     # combinations based on the amount of that character type and the total number of varieties of that character
-    if lowercase != 0:
-        lowercaseCombo = 26**lowercase
-    if uppercase != 0:
-        uppercaseCombo = 26**uppercase
-    if digits != 0:
-        digitsCombo = 10**digits
-    if punctuation != 0:
-        punctuationCombo = len(punctuationList)**punctuation
-    if symbols != 0:
-        symbolsCombo = len(symbolList)**symbols
-    if unidentified != 0:
-        unidentifiedCombo = (95 - 52 - 10 - len(punctuationList) - len(symbolList))**unidentified
-
-    # Calculates password integrity by finding the number of combinations of the password
-    totalCombos += lowercaseCombo + uppercaseCombo + digitsCombo + punctuationCombo + symbolsCombo + unidentifiedCombo
-    letterCombos += lowercaseCombo + uppercaseCombo
-    alphanumericCombos += lowercaseCombo + uppercaseCombo + digitsCombo
+    charset_size = 0
+    if lowercase: charset_size += 26
+    if uppercase: charset_size += 26
+    if digits: charset_size += 10
+    if punctuation: charset_size += len(punctuationList)
+    if symbols: charset_size += len(symbolList)
+    if unidentified: charset_size += unidentifiedCombo
+    totalCombos = charset_size ** len(password)
 
     #Checks if length of password is less than 15
-    if len(password) < 15:
-        print("Your password is less than 15 characters long. This is too short.\n")
+    if len(password) >= 15:
+        print(f"Your password is {len(password)} characters long. This is great!\n")
+        score += 3
+    else:
+        print(f"Your password is {len(password)} characters long. This is too short.\n")
 
     # Checks if the password contains only letters
     if password.isalpha():
         print("Your password contains only letters. This is acceptable.\n")
-        print(f"Your password has {letterCombos:,} possible combinations.\n")
-        crack_time(letterCombos)
     elif password.isdigit():
         print("Your password contains only digits. This is not good.\n")
-        print(f"Your password has {digitsCombo:,} possible combinations.\n")
-        crack_time(digitsCombo)
     elif password.isalnum():
         print("Your password contains only alphanumeric characters. This is acceptable.\n")
-        print(f"Your password has {alphanumericCombos:,} possible combinations.\n")
-        crack_time(alphanumericCombos)
     else:
         print("Your password contains a variety of characters. This is great!\n")
-        print(f"Your password has {totalCombos:,} possible combinations.\n")
-        crack_time(totalCombos)
+        score += 1
 
-    if not password.isdigit() and len(password) >= 15:
+    if is_phrase(password):
+        print("Your password is a passphrase.\n")
+        score += 2
+
+    print(f"Your password has {totalCombos:,} possible combinations.\n")
+    crack_time(totalCombos)
+
+    print(f"Your password has scored {score} out of 5 possible points.\n")
+
+    if score > 3:
         print(f"Your password {password} is acceptable.")
         break
 
